b39235bdb941623fb098f0b390ef33f53cbcb010
joychandrauday <joychandraud@gmail.com>
2024-02-26 18:56:11 +0600

    first commit

---
+++ js/json.js
@@ -0,0 +1,6 @@
+const person={name:'rahat',age:32};
+const stringified=JSON.stringify(person);
+const parseified=JSON.parse(stringified);
+console.log(person);
+console.log(stringified);
+console.log(parseified);
